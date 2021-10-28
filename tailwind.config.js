849a4a132bbc376ef22db2a06b16cfc51a4cfb8a
yujin <90164093+nayujina@users.noreply.github.com>
2021-10-28 10:30:19 +0900

    modal css

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,39 +61,5 @@ module.exports = {
     },
   },
   plugins: [
-    require('daisyui'),
   ],
-  daisyui: {
-    themes: [
-      {
-        'strumtheme' : {                     
-           'primary' : '#3B5FE0',           /* Primary color */
-           'primary-focus' : '#8462f4',     /* Primary color - focused */
-           'primary-content' : '#ffffff',   /* Foreground content color to use on primary color */
-
-           'secondary' : '#439CEF',         /* Secondary color */
-           'secondary-focus' : '#f3cc30',   /* Secondary color - focused */
-           'secondary-content' : '#ffffff', /* Foreground content color to use on secondary color */
-
-           'accent' : '#37cdbe',            /* Accent color */
-           'accent-focus' : '#2aa79b',      /* Accent color - focused */
-           'accent-content' : '#ffffff',    /* Foreground content color to use on accent color */
-
-           'neutral' : '#3d4451',           /* Neutral color */
-           'neutral-focus' : '#2a2e37',     /* Neutral color - focused */
-           'neutral-content' : '#ffffff',   /* Foreground content color to use on neutral color */
-
-           'base-100' : '#ffffff',          /* Base color of page, used for blank backgrounds */
-           'base-200' : '#f9fafb',          /* Base color, a little darker */
-           'base-300' : '#d1d5db',          /* Base color, even more darker */
-           'base-content' : '#1f2937',      /* Foreground content color to use on base color */
-
-           'info' : '#439CEF',              /* Info */
-           'success' : '#4BD059',           /* Success */
-           'warning' : '#F9B249',           /* Warning */
-           'error' : '#F35B5B',             /* Error */
-        },
-      },
-    ],
-  },
 }
